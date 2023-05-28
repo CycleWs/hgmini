c047069a02bb813cacb53e379f927391993e7c82
Fernando M Mattos <etcloide@gmail.com>
2023-05-28 14:05:13 -0300

    a

--- src/main/java/me/minihg/kits/Cultivator.java
+++ src/main/java/me/minihg/kits/Cultivator.java
@@ -23,38 +23,22 @@ public class Cultivator implements Listener {
     int value = 4;
     @EventHandler
     public void onPlace(BlockPlaceEvent e){
-        Player p = e.getPlayer();
-        if((KitSelector.kitMap.containsKey(p) && KitSelector.kitMap.get(p) == value) && Main.inGame){
-            if(e.getBlock().getType() == Material.SAPLING){
-                e.getBlock().setType(Material.AIR);
-                e.getPlayer().getWorld().generateTree(e.getBlock().getLocation(), TreeType.TREE);
-            }
-            else{
-                e.getBlock().setData((byte) 7);
-            }
-        }
-
-    }
-    @EventHandler
-    public void onInt(PlayerInteractEvent e){
-        Player p = e.getPlayer();
-        if((KitSelector.kitMap.containsKey(p) && KitSelector.kitMap.get(p) == value) && Main.inGame){
-            if(e.getClickedBlock() == null)
-                return;
-            Block b = e.getClickedBlock().getRelative(BlockFace.UP);
-            if(b.getType() == Material.CROPS){
-                BlockState blockState = b.getState();
-                MaterialData data = blockState.getData();
-                Crops crops = new Crops(data.getItemType(), data.getData());
-                crops.setState(CropState.RIPE);
-                blockState.setData(crops);
-                blockState.update(true);
+            Player p = e.getPlayer();
+            if((KitSelector.kitMap.containsKey(p) && KitSelector.kitMap.get(p) == value) && Main.inGame){
+                if(p.getItemInHand().getType() == Material.SAPLING){
+                    e.getBlock().setType(Material.AIR);
+                    e.getPlayer().getWorld().generateTree(e.getBlock().getLocation(), TreeType.TREE);
+                }
+                if(p.getItemInHand().getType() == Material.SEEDS){
+                    e.getBlock().setData((byte) 7);
+                }
+                if(p.getItemInHand().getType() == Material.INK_SACK){
+                    e.getBlock().setData((byte)11);
                 }
             }
         }
-    public static void getKitDescription(Player p){
-        p.sendMessage("§l§6Você recebeu o kit CULTIVATOR");
-        p.sendMessage("§aPlante uma semente e faça ela crescer instantaneamente");
-    }
+        public static void getKitDescription(Player p){
+            p.sendMessage("§l§6Você recebeu o kit CULTIVATOR");
+            p.sendMessage("§aPlante uma semente e faça ela crescer instantaneamente");
 
-}
+}}
